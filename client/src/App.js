c60ff8c6a97f9199873285d5e0e580a2bacd8d7c
Garrett Luu <garrettluu131@gmail.com>
2019-10-26 13:55:52 -0700

    Added scaffolding in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
+
+import Home from "./Home";
+import Login from "./Login";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <div id="header">
+
+          </div>
+
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About}/>
+            <Route exact path="/login" component={Login}/>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
