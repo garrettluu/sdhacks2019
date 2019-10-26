eb89c3516dd898fd1973f99c4a4a588dcbf4e699
fDirham <46035654+fDirham@users.noreply.github.com>
2019-10-26 14:19:02 -0700

    SO basically I added a header and a logo and link

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,71 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import Home from "./Pages/Home.js";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Logo from "./Images/logo.png";
+//import logo from './logo.svg';
+/*      <header className="App-header">
+        //<img src={logo} className="App-logo" alt="logo" /> */
+import "./App.css";
 
-import { BrowserRouter, NavLink, Route } from 'react-router-dom';
-
-import Home from "./Home";
-import Login from "./Login";
-
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <BrowserRouter>
-          <div id="header">
+      <Router>
+        <div className="App-Style">
+          <header className="Main-Header">
+            <div className="Header-Left">
+              <div className="Logo">
+                <img src={Logo} className="Logo" alt="logo" />
+              </div>
 
-          </div>
+              <nav className="Nav-Space">
+                <ul className="Nav-Bar">
+                  <li className>
+                    <Link to="/">
+                      {" "}
+                      <h1 className="Header-Text"> Home </h1>
+                    </Link>
+                  </li>
+                  <li className="Header-Text">
+                    <Link to="/about">
+                      <h1 className="Header-Text"> About </h1>
+                    </Link>
+                  </li>
+                  <li className="Header-Text">
+                    <Link to="/users">
+                      <h1 className="Header-Text"> Users </h1>
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </header>
 
-          <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/about" component={About}/>
-            <Route exact path="/login" component={Login}/>
-          </div>
-        </BrowserRouter>
-      </div>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
---
+++ client/src/Pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className="App-Content">
+        <p>
+          Hey so we did it right?
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    );
+  }
+}
