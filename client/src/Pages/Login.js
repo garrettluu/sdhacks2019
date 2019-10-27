e87f2a2175a9293947bea6981d792ba7bfb0f9d3
Garrett Luu <garrettluu131@gmail.com>
2019-10-26 18:36:32 -0700

    Tried to change color of text fields

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 import TextField from '@material-ui/core/TextField';
 
 import "../App.css";
 import "../Login.css";
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiInputLabel: {
+      root: {
+        color: 'white',
+        "&$focused": {
+          color: '#d8b65c'
+        }
+      }
+    },
+    MuiInput: {
+      underline: {
+        borderBottom: '2px solid white',
+        "&$focused": {
+          borderColor: '#d8b65c',
+          borderBottom: '2px solid #d8b65c',
+        }
+      }
+    }
+  }
+});
 class Login extends Component {
   render() {
     return (
@@ -11,14 +34,17 @@ class Login extends Component {
         <h1 id="login-header">
           Log In
         </h1>
-        <div className="input-field">
-          <TextField type="text" variant="outlined" label="Username"
-            placeholder="example123"/>
-        </div>
-        <div className="input-field">
-          <TextField type="password" variant="outlined" label="Password"
-            placeholder="hunter2"/>
-        </div>
+        <ThemeProvider theme={theme}>
+          <div className="input-field">
+            <TextField type="text" variant="standard" label="Username"
+              placeholder="example123" className="root"
+            />
+          </div>
+          <div className="input-field">
+            <TextField type="password" variant="standard" label="Password"
+              placeholder="hunter2" className="root"/>
+          </div>
+        </ThemeProvider>
 
       </div>
     );
